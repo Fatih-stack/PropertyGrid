3c67eb729a2caee9f96c8db4555b78db04190e1e
Fatih-stack <55389140+Fatih-stack@users.noreply.github.com>
2021-08-09 15:40:20 +0300

    Comments are added

--- PropertyGrdExample/PropertyGrdExampleDlg.cpp
+++ PropertyGrdExample/PropertyGrdExampleDlg.cpp
@@ -123,6 +123,8 @@ HCURSOR CPropertyGrdExampleDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+//take selected list item(NAME) and find corresponding values from map(Height,weight,age,gender)
+//insert selected item's keys and values to propertyGrid 
 void CPropertyGrdExampleDlg::OnLbnSelchangeList1()
 {
 	CListBox* pList1 = (CListBox*)GetDlgItem(IDC_LIST1);
@@ -130,21 +132,27 @@ void CPropertyGrdExampleDlg::OnLbnSelchangeList1()
 	nSel = pList1->GetCurSel();
 	if (nSel != LB_ERR)
 	{
-		pList1->GetText(nSel, ItemSelected);
+		pList1->GetText(nSel, ItemSelected);	//take selected item text as CString
 		CT2CA st(ItemSelected);
-		std::string s(st);
-		sItemSelected = s;
+		std::string s(st);				//Convert Cstring to std::string
+		sItemSelected = s;				//assign converted local value to class variable in order to use other func
 		CString gender = _T("Female");
 		CString str;
 		str.Format(L"%d", registers.find(s)->second[0]);
-		if(registers.find(s)->second[3] == 1) gender = _T("Male");
-		CMFCPropertyGridProperty* pProp3 = new CMFCPropertyGridProperty(_T("NAME"), ItemSelected);
+		if(registers.find(s)->second[3] == 1)	//gender value is saved as 1 or 0 (male : 1, female : 0)
+			gender = _T("Male");
+		//Item's keys and values are added to propertyGrid
+		//Item property NAME item value ItemSelected in propertyGrid
+		CMFCPropertyGridProperty* pProp3 = new CMFCPropertyGridProperty(_T("NAME"), ItemSelected);	
 		propertyGrid.AddProperty(pProp3);
+		//Item's next property Height item's value str in propertyGrid
 		pProp3 = new CMFCPropertyGridProperty(_T("Height"), str);
 		propertyGrid.AddProperty(pProp3);
+		//Item's next property Weight item's value str in propertyGrid, value is second element
 		str.Format(L"%d", registers.find(s)->second[1]);
 		pProp3 = new CMFCPropertyGridProperty(_T("Weight"), str);
 		propertyGrid.AddProperty(pProp3);
+		//Item's next property Age item's value str in propertyGrid, value is third element
 		str.Format(L"%d", registers.find(s)->second[2]);
 		pProp3 = new CMFCPropertyGridProperty(_T("Age"), str);
 		propertyGrid.AddProperty(pProp3);
@@ -154,37 +162,43 @@ void CPropertyGrdExampleDlg::OnLbnSelchangeList1()
 	}
 }
 
-
+//Remove selected item from list and map if select yes
 void CPropertyGrdExampleDlg::OnBnClickedDelbtn()
 {
+	//if choose YES remove item from list and map
 	if (MessageBox(NULL, _T("Are you sure to want to delete?") + ItemSelected, MB_YESNO) == IDYES) {
 		CT2CA st(ItemSelected);
-		std::string s(st);
+		std::string s(st);		//convert CString to std::string
 		m_listBox.DeleteString(nSel);
 		auto it = registers.find(s);
 		registers.erase(it);
 	}
 }
 
-
+//Takes values of edit controls and assign them to corresponding variables
+//and insert it to map and add item's name to list
 void CPropertyGrdExampleDlg::OnBnClickedSavebtn()
 {
+	//Variables for edit controls
 	wchar_t name[25],  height[4],  weight[4], age[4];
+	//Take values of edit controls and assign corresponding variables
 	GetDlgItemText(IDC_Name, name, 16);
 	CString NAME = name;
 	CT2CA st(NAME);
-	std::string s(st);
+	std::string s(st);						//CString to std::string
 	GetDlgItemText(IDC_Height, height, 16);
-	int hgt = _wtoi(height);
+	int hgt = _wtoi(height);				//wchar_t to int
 	GetDlgItemText(IDC_Weight, weight, 16);
 	int wgt = _wtoi(weight);
 	GetDlgItemText(IDC_Age, age, 16);
 	int Age = _wtoi(age);
 	int gender = comboGender.GetCurSel();
 	std::vector<int> values;
+	//insert Name as key and value as vector consist of corresponding values
 	registers.insert(std::make_pair(s, std::vector<int>{hgt, wgt, Age, gender}));
+	//add name to listbox
 	m_listBox.AddString(NAME);
-	//clear all edit lists
+	//clear all edit controls
 	SetDlgItemText(IDC_Name, L"");
 	SetDlgItemText(IDC_Height, L"");
 	SetDlgItemText(IDC_Weight, L"");
@@ -200,23 +214,21 @@ void CPropertyGrdExampleDlg::OnStnClickedMfcpropertygrid1()
 
 void CPropertyGrdExampleDlg::OnStnEnableMfcpropertygrid1()
 {
-	CMFCPropertyGridProperty* pProperty;//Define a pointer to the child
-	pProperty = propertyGrid.GetCurSel();
-	const COleVariant& strValue = pProperty->GetValue();
-	CString strTmp = (CString)strValue;
 }
 
-
+//Click edited key and take edited value and change value in the map
 void CPropertyGrdExampleDlg::OnStnDblclickMfcpropertygrid1()
 {
 	CMFCPropertyGridProperty* pProperty;//Define a pointer to the child
-	pProperty = propertyGrid.GetCurSel();
-	CString str = pProperty->GetName();
-	COleVariant strValue = pProperty->GetValue();
+	pProperty = propertyGrid.GetCurSel();	//take selection
+	CString str = pProperty->GetName();		//take key
+	COleVariant strValue = pProperty->GetValue();	//take value
 	CString strTmp = (CString)strValue;
+	//assign taken value to corresponding place according to key
+	//if name is edited assign all values to new name and remove old name
 	if (str == "NAME") {
-		std::vector<int> vals{ registers[sItemSelected] };
-		registers.erase(sItemSelected);
+		std::vector<int> vals{ registers[sItemSelected] };	//assign values of selected name to vals variable
+		registers.erase(sItemSelected);		//remove selected item from map
 		ItemSelected = strTmp;
 		CT2CA st(ItemSelected);
 		std::string s(st);
@@ -224,6 +236,7 @@ void CPropertyGrdExampleDlg::OnStnDblclickMfcpropertygrid1()
 		registers[sItemSelected] = vals;
 	}
 	else {
+		//if value is changed update it from values according to value's index
 		if (str == "Height") {
 			registers.find(sItemSelected)->second[0] = _wtoi(strTmp);
 		}
@@ -236,18 +249,22 @@ void CPropertyGrdExampleDlg::OnStnDblclickMfcpropertygrid1()
 	}
 }
 
-
+//If file is not exist create file and write chosen item to this file ( as Name;175;75;20;Male)
+//If exist add chosen item to file
 void CPropertyGrdExampleDlg::OnBnClickedExpbtn()
 {
-	// TODO: Add your control notification handler code here
-	CFile FileObj;
-	FileObj.Open(L"ArchiveText.txt", CFile::modeCreate | CFile::modeWrite | CFile::modeNoTruncate);
+	CFile FileObj;		//Variable for file operations
+	//Open existing file or it doesn't exist create and open
+	FileObj.Open(L"Registers.txt", CFile::modeCreate | CFile::modeWrite | CFile::modeNoTruncate);
 	CString height, weight, age, gender;
+	//Convert integer values to CString and assign corresponding variables
 	height.Format(L"%d", registers.find(sItemSelected)->second[0]);
 	weight.Format(L"%d", registers.find(sItemSelected)->second[1]);
 	age.Format(L"%d", registers.find(sItemSelected)->second[2]);
+	//Determine gender according to value in the last(gender) index
 	if (registers.find(sItemSelected)->second[3] == 1) gender = "Male";
 	else gender = "Female";
+	//Create CString variable according to given format to write file
 	CString str = _T("") + ItemSelected + _T(";") + height + _T(";") + weight + _T(";") + 
 		age + _T(";") + gender + _T("\r\n");
 	FileObj.SeekToEnd();
